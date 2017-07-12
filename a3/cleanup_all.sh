60f3346637a4dd28f48431289d78f8ca3a2e04a6
j33lai <j33lai@uwaterloo.ca>
2017-07-12 12:59:56 -0400

    Remove sh files

--- a3/cleanup_all.sh
+++
@@ -1,7 +0,0 @@
-ssh a9palmer@ubuntu1404-002.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-ssh a9palmer@ubuntu1404-004.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-ssh a9palmer@ubuntu1404-006.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-ssh a9palmer@ubuntu1404-008.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-ssh a9palmer@ubuntu1404-010.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-ssh a9palmer@ubuntu1404-012.student.cs.uwaterloo.ca 'bash ~/CS454/a3/cleanup.sh'
-
--- a3/set_env_vars.sh
+++
@@ -1,9 +0,0 @@
-# Set the env variables for this server. Waits for the file to be written to. 
-# todo: add a timeout here
-if [ ! -f ~/a3_env_vars ] ; then
-    echo "Couldn't find the env vars file!"
-    exit
-fi
-
-while [ `cat ~/a3_env_vars | grep BINDER_PORT | wc -l` -lt 1 ]; do :; done
-source ~/a3_env_vars
